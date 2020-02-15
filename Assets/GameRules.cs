3bbd64c614e243e15d14cceb0b9ee54c27c5ea10
Kubilay <kubi0707@gmail.com>
2020-02-15 04:06:46 +0300

    qwe
    qwe

---
+++ Assets/GameRules.cs
@@ -0,0 +1,69 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameRules : MonoBehaviour
+{
+
+    
+  
+    public GameObject ground;
+    public GameObject ball;
+    public Text txt;
+    bool round;
+    Canvas canv;
+    // Start is called before the first frame update
+    void Start()
+    {        
+        round = false;
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    private void OnCollisionEnter2D(Collision2D coll)
+    {
+
+        Debug.Log("Top " + coll.gameObject.name + "'e çaptı");
+        txt.text = "Naber";
+        
+        if (round == false && coll.gameObject.name=="RedSide")
+        {
+            round = true;
+            StartCoroutine(resetGame("RedSide"));
+        }
+        
+    }
+    void resetBallPosition()
+    {
+        
+    }
+
+
+    IEnumerator resetGame(string side)
+    {        
+        if (side=="RedSide")
+        {
+            Debug.Log("Bekleme başladı.");
+            yield return new WaitForSeconds(3);
+            Debug.Log("Mavi puan kazandı!");
+            Debug.Log(round);
+        }
+        
+        else if(side =="BlueSide")
+        {
+            yield return new WaitForSeconds(3);
+            Debug.Log("Kırmızı puan kazandı!");
+            Debug.Log(round);
+        }
+    }
+
+
+   
+
+
+}
---
+++ Assets/Movement.cs
@@ -0,0 +1,109 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+    Rigidbody2D rb;
+    Vector2 jmpForce = new Vector2(0f, 20f);
+    public CharacterController2D controller;
+
+    public float runSpeed = 40f;
+
+    float horizontalMove = 0f;
+    bool jump = false;
+    bool crouch = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+
+        horizontalMove = Input.GetAxisRaw("Horizontal") * runSpeed;
+
+        if (Input.GetButtonDown("Jump"))
+        {
+            jump = true;
+        }
+
+        /*if (Input.GetButtonDown("Crouch"))
+        {
+            crouch = true;
+        }
+        else if (Input.GetButtonUp("Crouch"))
+        {
+            crouch = false;
+        }*/
+
+    }
+
+  
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    //-------------------
+
+    /*rb = GetComponent<Rigidbody2D>();
+    transform.Translate(Input.GetAxis("Horizontal") *7* Time.deltaTime, 0f, 0f);
+    //transform.Translate(0f, Input.GetAxis("Vertical") * 20* Time.deltaTime, 0f);
+    Debug.Log(Input.GetAxis("Vertical"));
+    if (Input.GetAxisRaw("Vertical")>0f)
+    {
+        rb.AddForce(jmpForce*Time.deltaTime);
+
+    }*/
+
+
+
+
+
+
+void FixedUpdate()
+{
+    // Move our character
+    controller.Move(horizontalMove * Time.fixedDeltaTime, crouch, jump);
+    jump = false;
+}
+
+
+
+/*private void FixedUpdate()
+    {
+
+        rb = GetComponent<Rigidbody2D>();
+        transform.Translate(Input.GetAxis("Horizontal") * 7 * Time.deltaTime, 0f, 0f);
+        //transform.Translate(0f, Input.GetAxis("Vertical") * 20* Time.deltaTime, 0f);
+        Debug.Log(Input.GetAxis("Vertical"));
+
+        if (Input.GetAxisRaw("Vertical") > 0f)
+        {
+            rb.AddForce(jmpForce * Time.deltaTime);
+
+        }
+    }*/
+
+
+}
