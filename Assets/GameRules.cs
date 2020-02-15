ecb8fabba26c7382d999b5f6511d204d8df8b6d3
Kubilay <kubi0707@gmail.com>
2020-02-15 06:02:43 +0300

    Top mekanikleri geliştirildi
    Top artık yere çarptığında 3 saniye sonra bir sonraki round için pozisyonu resetleniyor.
    Sadece red side için yapıldı.
    Oyun ekranı genişletildi.
    Yer spriteları çıkartıldı.

--- Assets/GameRules.cs
+++ Assets/GameRules.cs
@@ -12,30 +12,45 @@ public class GameRules : MonoBehaviour
     public GameObject ball;
     public Text txt;
     bool round;
+    bool roundEnd;
     Canvas canv;
+    Vector2 temp;
+    
+    Vector2 startPoint = new Vector2(-4.18f, 0.99f);
+    Rigidbody2D rb;
+    
     // Start is called before the first frame update
     void Start()
     {        
         round = false;
+        rb = GetComponent<Rigidbody2D>();
 
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        Debug.Log(transform.rotation);
     }
     private void OnCollisionEnter2D(Collision2D coll)
     {
 
         Debug.Log("Top " + coll.gameObject.name + "'e çaptı");
         txt.text = "Naber";
+        if (roundEnd == true)
+        {
+            roundEnd = false;
+            rb.gravityScale = 0.5f;
+            rb.freezeRotation = false;
+            
+        }
         
-        if (round == false && coll.gameObject.name=="RedSide")
+        if (round == false && coll.gameObject.name=="RedSide" && roundEnd==false)
         {
             round = true;
             StartCoroutine(resetGame("RedSide"));
         }
+        
         
     }
     void resetBallPosition()
@@ -49,9 +64,17 @@ public class GameRules : MonoBehaviour
         if (side=="RedSide")
         {
             Debug.Log("Bekleme başladı.");
-            yield return new WaitForSeconds(3);
+            yield return new WaitForSeconds(3);  //3 saniye bekle
             Debug.Log("Mavi puan kazandı!");
-            Debug.Log(round);
+            Debug.Log("Puan Ekle Topu yerleştir.");
+            rb.gravityScale = 0;
+            rb.velocity = Vector3.zero;
+            //transform.rotation = Quaternion.identity;
+            rb.freezeRotation = true;
+            transform.position = startPoint;
+            roundEnd = true;
+            round = false;
+            
         }
         
         else if(side =="BlueSide")
